0bfc1d0817a522f77480d071f424e6782aa7b763
gonato-janalexalouisse <alexagonato@gmail.com>
2025-06-03 23:57:50 +0800

    Add gonato profile. (#4)
    Co-authored-by: gonato-janalexalouissee <alexagonato@gmail.comm>

---
+++ gwn10package/gonato.py
@@ -0,0 +1,23 @@
+def gonato_profile():
+    """ Display Alexa Gonato's profile information. """
+    while True:
+        print("\n=== Hi! Welcome to Jan Alexa Louisse P. Gonato's Personal Menu ===")
+        print("[1] Alexa's Basic Info")
+        print("[2] Alexa's Goals")
+        print("[0] Return to Main Menu")
+
+        choice = input("Please enter your choice: ").strip()
+
+        match choice:
+            case "1":
+                print("\nAlexa's Basic Info")
+                print("I am an IT student from BSIT 2-1.")
+            case "2":
+                print("\nAlexa's Goals")
+                print("To finally stop saying ‘student pa lang po’ in every intro.")
+            case "0":
+                print("\nReturning to Main Menu")
+                print("Goodbye champ! PEACE OUT!")
+                break
+            case _:
+                print("\nInvalid choice. Please try again.")
